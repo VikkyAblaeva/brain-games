b367bfeb67173ed3be510aac509154b4358a762c
VikkyAblaeva <nifergirl@yandex.ru>
2022-06-06 21:01:38 +0300

    step 5.1

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-import { greeting, userName } from '../src/cli.js';
+import { greeting } from '../src/cli.js';
 
-const even = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let result = '';
-  let userQuestion = 0;
-  let yesOrNo = '';
-  let exit = '';
-  for (let i = 1; i < 4; i += 1) {
-    userQuestion = Math.floor(Math.random() * 100);
-    console.log(`Question: ${userQuestion}`);
-    result = readlineSync.question('You answer: ');
-    if (userQuestion % 2 === 0) {
-      yesOrNo = 'yes';
-    } else {
-      yesOrNo = 'no';
-    }
-    if (
-      (result === 'yes' && userQuestion % 2 === 0)
-      || (result === 'no' && userQuestion % 2 !== 0)
-    ) {
-      console.log('Correct!');
-    } else {
-      exit = `'${result}' is wrong answer ;(. Correct answer was '${yesOrNo}'\nLet's try again, ${userName}!`;
-      return exit;
-    }
-  }
-  return `Congratulations, ${userName}!`;
-};
+import { even } from '../src/cliEven.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(greeting());
---
+++ src/cliEven.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+import { userName } from '../src/cli.js';
+
+const even = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let result = '';
+  let userQuestion = 0;
+  let yesOrNo = '';
+  let exit = '';
+  for (let i = 1; i < 4; i += 1) {
+    userQuestion = Math.floor(Math.random() * 100);
+    console.log(`Question: ${userQuestion}`);
+    result = readlineSync.question('You answer: ');
+    if (userQuestion % 2 === 0) {
+      yesOrNo = 'yes';
+    } else {
+      yesOrNo = 'no';
+    }
+    if (
+      (result === 'yes' && userQuestion % 2 === 0) ||
+      (result === 'no' && userQuestion % 2 !== 0)
+    ) {
+      console.log('Correct!');
+    } else {
+      exit = `'${result}' is wrong answer ;(. Correct answer was '${yesOrNo}'\nLet's try again, ${userName}!`;
+      return exit;
+    }
+  }
+  return `Congratulations, ${userName}!`;
+};
+
+export { even };
