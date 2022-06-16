e562854ad004991a2474fcf321a386e4480a13a9
VikkyAblaeva <nifergirl@yandex.ru>
2022-06-16 23:15:24 +0300

    fix magic numbers

--- src/games/prime.js
+++ src/games/prime.js
@@ -11,12 +11,13 @@ import {
 const primeOrNot = () => {
   let userAnswer;
   initialGreeting();
+  const exceptions = 2;
   const rangeOfRandom = 20;
   const userName = readlineSync.question('May I have your name? ');
   greetingWithName(userName);
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
-    const userNumber = random(rangeOfRandom);
+    const userNumber = random(rangeOfRandom) + exceptions;
     console.log(`Question: ${userNumber}`);
     userAnswer = readlineSync.question('Your answer: ');
     if ((isPrime(userNumber) === 'yes' && userAnswer === 'yes') || (isPrime(userNumber) === 'no' && userAnswer === 'no')) {
